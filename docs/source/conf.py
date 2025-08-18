c9df6ec99606270fac31728332b03d425cb5b326
ghadiben0 <ghadiben099@gmail.com>
2025-08-18 15:05:01 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+html_theme_options = {
+    "footer_text": "Your Online Documentation hub is your knowledge source to better know application functionality"
+}
