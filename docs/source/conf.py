a7f7abe11d891b8972bfcb8d24c7057dcc054a5d
ghadiben0 <ghadiben099@gmail.com>
2025-08-18 15:48:37 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ html_theme_options = {
 
 }
 html_static_path = ['_static']
-html_css_files = ['custom.css']
+html_js_files = ['custom.js']
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
