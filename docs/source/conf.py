a744fe7b0e8ee73dcc82779712b7f37703576c96
ghadiben0 <ghadiben099@gmail.com>
2025-08-20 11:00:23 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,9 +44,11 @@ html_js_files = ['custom.js']
 epub_show_urls = 'footnote'
 
 html_theme_options = {
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "titles_only": False,
+    "show_relbars": True,
+    "style_external_links": True,
+    "display_version": True,
+    "show_footer": False   # hides default "Built with Sphinx" line
 }
-
-
-
-
-
