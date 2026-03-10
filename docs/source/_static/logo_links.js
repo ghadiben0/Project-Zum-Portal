7f2a89abeaf9848da35531c0476eafcb080cd3f6
Ghada Ben Nasser <ghadiben099@gmail.com>
2026-03-10 12:26:04 +0100

    Update logo link in RTD header

--- docs/source/_static/logo_links.js
+++ docs/source/_static/logo_links.js
@@ -1,1 +1,7 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    // Select the logo link in RTD header
+    const logoLink = document.querySelector(".wy-side-nav-search a");
+    if (logoLink) {
+        logoLink.href = "https://https://www.zum-it.com/";
+    }
+});
